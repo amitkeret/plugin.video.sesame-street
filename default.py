1cab80669193344e857759039938e499403c6d98
Amit Keret <amitkeretwork@gmail.com>
2014-01-18 14:52:30 +0200

    new page: muppets. no video list implementation yet

--- default.py
+++ default.py
@@ -4,12 +4,15 @@ import json
 import xbmcgui
 import xbmcplugin
 
+from BeautifulSoup import BeautifulSoup as bsoup
+
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 
 sesame_base_domain = 'sesamestreet.org'
 sesame_base_url = 'http://www.' + sesame_base_domain
+sesame_m_base_url = 'http://m.' + sesame_base_domain
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
@@ -22,49 +25,79 @@ def log(txt):
 def build_url(query):
   return base_url + '?' + urllib.urlencode(query)
 
-post_data = {
-  'serviceClassName': 'org.sesameworkshop.service.UmpServiceUtil',
-  'serviceMethodName': 'getMediaItems',
-  'serviceParameters': ["criteria","capabilities","resultsBiasingPolicy","context"],
-  'criteria': {
-    "qty":25,
-    "reset": True,
-    "type": "video"
-  },
-  'capabilities': {},
-  'resultsBiasingPolicy': '',
-  'context': {}
-}
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-req = urllib2.Request(sesame_base_url + '/c/portal/json_service', urllib.urlencode(post_data), headers)
-videos_data = json.load(urllib2.urlopen(req))
+page = args.get('page', None)
+if (page):
+  page = page[0]
 
-for video in videos_data['content']:
-  if len(video['source']) == 0:
-    pass
+if page == 'topics':
+  pass
+
+elif page == 'recent':
+  post_data = {
+    'serviceClassName': 'org.sesameworkshop.service.UmpServiceUtil',
+    'serviceMethodName': 'getMediaItems',
+    'serviceParameters': ["criteria","capabilities","resultsBiasingPolicy","context"],
+    'criteria': {
+      "qty":25,
+      "reset": True,
+      "type": "video"
+    },
+    'capabilities': {},
+    'resultsBiasingPolicy': '',
+    'context': {}
+  }
+  headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  req = urllib2.Request(sesame_base_url + '/c/portal/json_service', urllib.urlencode(post_data), headers)
+  videos_data = json.load(urllib2.urlopen(req))
   
-  videoFile = video['source'][0]
-  url = videoFile['fileName']
+  for video in videos_data['content']:
+    if len(video['source']) == 0:
+      pass
+    
+    videoFile = video['source'][0]
+    url = videoFile['fileName']
+    
+    li = xbmcgui.ListItem(video['title'])
+    li.setInfo('video', {
+      'count': video['sesameItemId'],
+      'codec': videoFile['codec'],
+      'plot': video['description'],
+      'plotoutline': video['description'],
+      'cast': video['character'].split(';'),
+    })
+    li.setIconImage(sesame_base_url + video['thumbnailSmall'])
+    li.setThumbnailImage(sesame_base_url + video['thumbnailLarge'])
+    li.addStreamInfo('video', {
+      'codec': videoFile['codec'],
+      'width': video['width'],
+      'height': video['height'],
+      'aspect': video['width'] / video['height']
+    })
+    li.setProperty('fanart_image', sesame_base_url + video['poster'])
+    
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+elif page == 'muppets':
+  html = urllib.urlopen(sesame_m_base_url + '/muppets')
+  result = html.read()
+  lis = bsoup(result).find('ul', {'id':'muppet-slideshow'}).findAll('li', {'class':'section'})
   
-  li = xbmcgui.ListItem(video['title'])
-  li.setInfo('video', {
-    'count': video['sesameItemId'],
-    'codec': videoFile['codec'],
-    'plot': video['description'],
-    'plotoutline': video['description'],
-    'cast': video['character'].split(';'),
-  })
-  li.setIconImage(sesame_base_url + video['thumbnailSmall'])
-  li.setThumbnailImage(sesame_base_url + video['thumbnailLarge'])
-  li.addStreamInfo('video', {
-    'codec': videoFile['codec'],
-    'width': video['width'],
-    'height': video['height'],
-    'aspect': video['width'] / video['height']
-  })
-  li.setProperty('fanart_image', sesame_base_url + video['poster'])
-  
-  xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+  for item in lis:
+    log(item.a.img['src'])
+    
+    m_name = item.a['href'][item.a['href'].index('/muppets/') + len('/muppets/'):]
+    m_name_pretty = ' '.join(m_name.split('-')).title()
+    
+    li = xbmcgui.ListItem(m_name_pretty)
+    li.setIconImage(item.a.img['src'])
+    li.setThumbnailImage(item.a.img['src'])
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url='test', listitem=li)
+    
+else:
+  li = xbmcgui.ListItem('Recent videos', iconImage='DefaultFolder.png')
+  xbmcplugin.addDirectoryItem(handle=addon_handle, url=build_url({'page':'recent'}), listitem=li, isFolder=True)
+  li = xbmcgui.ListItem('Muppets', iconImage='DefaultFolder.png')
+  xbmcplugin.addDirectoryItem(handle=addon_handle, url=build_url({'page':'muppets'}), listitem=li, isFolder=True)
 
 
 xbmcplugin.endOfDirectory(addon_handle)
